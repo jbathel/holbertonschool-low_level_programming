9ffb1597ed9b832d861e733a6cf5053be5a01ede
jbathel <811@holbertonschool.com>
2019-06-19 20:13:44 +0000

    Updating to correct betty format

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 
 /**
- * main - Entry point
- *
+ * main - entry point
+ * Description: prints characters in an array
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
-	returns (0);
+	return (0);
 }
