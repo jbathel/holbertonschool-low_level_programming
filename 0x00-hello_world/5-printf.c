f143b577a069bef99b41d1b09ce1a5cfb5bfee65
jbathel <811@holbertonschool.com>
2019-06-19 20:28:15 +0000

    Updating to correct betty format

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 
 /**
- * main - Entry point
+ * main - entry point
  *
+ * Description: prints characters in an array
  * Return: Always 0 (Success)
  */
 int main(void)
 {
         printf("with proper grammar, but the outcome is a piece of art,");
-        returns (0);
+        return (0);
 }
