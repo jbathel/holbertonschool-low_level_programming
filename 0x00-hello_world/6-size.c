35c7cf836a1a5ededb187e63dfa5f02c4db1e01e
jbathel <811@holbertonschool.com>
2019-06-19 21:49:41 +0000

    Updating file 6-size.c to first version

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,18 @@
-#!/bin/bash
+#include<stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Prints size of various types
+ * on the computer it is compiled and run on.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("%d\n", "Size of a char:" sizeof(char));
+printf("%d\n", "Size of an int:" sizeof(int));
+printf("%d\n", "Size of a long int:" sizeof(long int));
+printf("%d\n", "Size of a long long int:" sizeof(long long int));
+printf("%d\n", "Size of a float:" sizeof(float));
+return (0);
+}
