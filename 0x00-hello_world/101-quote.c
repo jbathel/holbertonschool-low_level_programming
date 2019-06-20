a67531c74218b1d2b831f88150b778441e7f1fb7
jbathel <811@holbertonschool.com>
2019-06-20 00:57:25 +0000

    Adding version 1 of file 101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<unistd.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints characters in an array
+ * without using printf or putc
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+write(1,"\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",65);
+return (1);
+}
