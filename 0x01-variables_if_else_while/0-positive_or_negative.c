a5f54dcefc10c50a71ca9d01c96c41cf171aab09
jbathel <811@holbertonschool.com>
2019-06-20 22:29:29 +0000

    Update to version 2 including proper headers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /**
@@ -23,7 +24,7 @@ n = rand() - RAND_MAX / 2;
 	}
 	if (n < 0)
 	{
-	printf("%d is negativen\n", n);
+	printf("%d is negative\n", n);
 	}
 return (0);
 }
