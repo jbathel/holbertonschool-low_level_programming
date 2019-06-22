d76c911ea7c29186db0728f23bff472d5a243ce0
jbathel <811@holbertonschool.com>
2019-06-22 22:54:57 +0000

    Adding file 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: Prints all single digit numbers
+ * of base 10 starting from 0, followed by a new line
+ * using only putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char a;
+
+	for (a = 48; a <= 57; a++)
+	{
+	putchar(a);
+	}
+	putchar('\n');
+return (0);
+}
