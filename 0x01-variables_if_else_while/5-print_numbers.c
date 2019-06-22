a0e6cc3805a75cc66ba4b7450fd0de41d6d36275
jbathel <811@holbertonschool.com>
2019-06-22 22:19:58 +0000

    Adding file 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: Prints all single digit numbers
+ * of base 10 starting from 0, followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a = 0;
+
+	while (a < 10)
+	{
+	printf("%d\n", a++);
+	}
+return (0);
+}
