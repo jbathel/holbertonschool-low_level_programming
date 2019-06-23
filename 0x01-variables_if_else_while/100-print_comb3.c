d37c0558d280be2c0f2a7432e2e80f9c547e03b9
jbathel <811@holbertonschool.com>
2019-06-23 23:30:43 +0000

    Adding 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description:
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a;
+int b;
+	for (a = 48; a <= 57; a++)
+	{
+		for (b = 48; b <= 57; b++)
+		{
+				if (!(a == b || b <= a))
+				{
+				putchar(a);
+				putchar(b);
+				if (!(a == 56 && b == 57))
+				{
+				putchar(44);
+				putchar(32);
+				}
+				}
+		}
+	}
+	putchar('\n');
+return (0);
+}
