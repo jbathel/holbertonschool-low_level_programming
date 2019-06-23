6368fbbda36849e03f5c1ea51b7c8125f92102c7
jbathel <811@holbertonschool.com>
2019-06-23 23:32:54 +0000

    Adding 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,16 +14,16 @@ int b;
 	{
 		for (b = 48; b <= 57; b++)
 		{
-				if (!(a == b || b <= a))
-				{
-				putchar(a);
-				putchar(b);
-				if (!(a == 56 && b == 57))
-				{
-				putchar(44);
-				putchar(32);
-				}
-				}
+			if (!(a == b || b <= a))
+			{
+			putchar(a);
+			putchar(b);
+			if (!(a == 56 && b == 57))
+			{
+			putchar(44);
+			putchar(32);
+			}
+			}
 		}
 	}
 	putchar('\n');
