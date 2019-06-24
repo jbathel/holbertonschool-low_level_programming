0f6deb55026fbd53df08bdab475b9154a2eea646
jbathel <811@holbertonschool.com>
2019-06-24 21:59:31 +0000

    Adding file 0-holberton.c

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * main - entry point
+ *
+ * Description: This program will assign a random number
+ * to the variable n each time it is executed.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char a[] = "Holberton";
+int b;
+
+	for (b = 0; b <= 10; b++)
+	_putchar (a[b]);
+	_putchar ('\n');
+return (0);
+}
