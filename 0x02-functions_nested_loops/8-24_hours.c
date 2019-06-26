718ae85196a11f8c89535742ee690456f08017bc
jbathel <811@holbertonschool.com>
2019-06-26 15:28:00 +0000

    Updating holberton.h

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+/**
+ * jack_bauer(void) - calls function
+ *
+ * Description:
+ *
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+int h, m;
+
+for(h = 0; h <=23; h++)
+	_putchar(h);
+
+	_putchar(':');
+	for(m = 0; m <=59; m++)
+	_putchar(m);
+
+}
+
+int h;
+int hh;
+int m;
+int mm;
+
+	for (h = 0; h <= 23; h++)
+	{
+		for (hh = 0; hh <= 23; hh++)
+		{
+			putchar(h);
+			putchar(hh);
+			if (!(a == 57 && b == 57))
+			{
+			putchar(44);
+			putchar(32);
+			}
+		}
+	}
+	putchar('\n');
+return (0);
+}
+
