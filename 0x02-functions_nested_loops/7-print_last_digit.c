0d57c35367cff349da4a726d82c79c8e83f296fe
jbathel <811@holbertonschool.com>
2019-06-26 15:25:13 +0000

    Adding 7-print_last_digit.c removing int n and replacing with r moved mod

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,16 +1,17 @@
 #include "holberton.h"
+
 /**
  * print_last_digit - calls function
  * @r: Integer to be computed
  * Description: Function that prints the
  * last digit of number.
- * Return: 0
+ * Return: r.
  */
 int print_last_digit(int r)
 {
-r = r % 10;
-if (r < 0)
-	r = -r;
-_putchar('0' + r);
-return (r);
+	r = r % 10;
+	if (r < 0)
+		r = -r;
+	_putchar ('0' + r);
+	return (r);
 }
