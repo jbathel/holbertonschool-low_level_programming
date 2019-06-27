5607176954cc53c2979d7069d9ff6095830785ff
jbathel <811@holbertonschool.com>
2019-06-27 03:51:37 +0000

    Updating holberton.h to file provided in task 0

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -1,18 +1,6 @@
-int _putchar(char c);
-int main(void);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int n);
-int print_last_digit(int r);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
-void print(int n);
-void print_one(int n);
-void print_two(int n);
-void print_three(int n);
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
