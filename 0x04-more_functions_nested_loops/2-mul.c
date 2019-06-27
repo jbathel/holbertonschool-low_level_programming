dfb7606bb9cf200ed1cb141805359641f4e2be7a
jbathel <811@holbertonschool.com>
2019-06-27 17:38:24 +0000

    Updating file 2-mul.c to version 1

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * mul - calls function
+ * @a: Digit one
+ * @b: Digit two
+ * Description: Function that
+ * multplies a and b.
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
+
