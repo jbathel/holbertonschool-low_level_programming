133dc3eef1f8df58a7d5e9e82687a3f02be5b0f0
jbathel <811@holbertonschool.com>
2019-06-27 21:05:09 +0000

    Updating file 5-more_numbers.c to first version

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * more_numbers - calls function
+ *
+ * Description: Prints 10 times the numbers,
+ * from 0 to 14, followed by a new line.
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			if (b > 9)
+			_putchar ('0' + b / 10);
+			_putchar ('0' + b % 10);
+		}
+		_putchar ('\n');
+	}
+}
