9bc414a8177bbfda8221bde9d21781871e8b88c9
jbathel <811@holbertonschool.com>
2019-06-27 20:02:50 +0000

    Updating 4-priont_most_numbers.c to version 1

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * print_most_numbers - calls function
+ *
+ * Description: Prints numbers 0 through
+ * 9, except 2 and 4.
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		if (a != 50 && a != 52)
+		_putchar (a);
+	}
+	_putchar ('\n');
+}
