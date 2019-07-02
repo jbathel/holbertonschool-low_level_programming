a159faeda7284b6068cac630a4ec11ca1f1b1984
jbathel <811@holbertonschool.com>
2019-07-02 16:41:19 +0000

    Adding intial files for Project 0x05

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - calls function
+ * @n: pointer variable to reset
+ * Description: Function that takes a pointer to an int as
+ * parameter and updates the value it points to to 98.
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
