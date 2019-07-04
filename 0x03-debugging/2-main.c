8c61fbe111e8edc757e5d932ca8aa5b91efba55e
jbathel <811@holbertonschool.com>
2019-07-04 22:08:57 +0000

    Updating files to test more edge cases

---
+++ 0x03-debugging/2-main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+  * main - prints the largest of 3 integers
+  * Return: 0
+  */
+
+int main(void)
+{
+	int a, b, c;
+	int largest;
+
+	a = 98347;
+	b = 98346;
+	c = 98345;
+
+	largest = largest_number(a, b, c);
+	printf("%d is the largest number\n", largest);
+
+	return (0);
+}
