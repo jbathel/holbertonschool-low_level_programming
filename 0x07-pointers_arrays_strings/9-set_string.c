d349cf79da3e71b1b23ad15f6ac410bb3f1811de
jbathel <811@holbertonschool.com>
2019-07-10 03:18:58 +0000

    Adding task 9 for checker

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * set_string - calls function
+ * @s: original string
+ * @to: string to compare
+ * Description Write a function that sets the value of a pointer to a char.
+ * Return: 0
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
