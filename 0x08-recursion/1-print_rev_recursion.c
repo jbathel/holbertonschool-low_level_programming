acac7e0a98e47fec75cdf38e87e8983db8bb437c
jbathel <811@holbertonschool.com>
2019-07-11 01:22:43 +0000

    Updating file 1-print_rev_recursion.c to version 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _print_rev_recursion - calls function
+ * @s: original string
+ * Description: Function that prints a string
+ * in reverse.
+ * Return: 0
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar (*s);
+}
