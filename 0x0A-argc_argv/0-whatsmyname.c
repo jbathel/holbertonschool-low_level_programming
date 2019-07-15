56e199c7fac39f228756b2e662fcba5bf7c3f269
jbathel <811@holbertonschool.com>
2019-07-15 00:49:29 +0000

    Uploading 0-whatsmyname.c version 1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: arguments passed through command line
+ * Description: Prints characte program that prints its name,
+ * followed by a new line.
+ * Return: Always 0 (Success)
+ */
+int main (int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
