b3d982c2ccca82a1df551cce04867454faeab6e9
jbathel <811@holbertonschool.com>
2019-07-15 21:42:25 +0000

    Updating file 1-args.c to version 2

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: arguments passed through command line
+ * Description: Program that prints its name,
+ * followed by a new line.
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
