fce498719f8747eac07fde81ac52b847c7bac7c6
jbathel <811@holbertonschool.com>
2019-07-22 17:58:38 +0000

    Uploading file 3-function_like_macro.h version 1 to repo

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Write a function-like macro ABS(x) that computes the absolute value of a number x. */
+
+#define ABS(x) ((x) * (-1))
+
+#endif /* HOLBERTON_H */
