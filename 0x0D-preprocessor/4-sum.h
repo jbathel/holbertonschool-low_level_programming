7fd6c14f4373c3fe5b7f2fbd8a87a35eae7a3d58
jbathel <811@holbertonschool.com>
2019-07-22 18:06:49 +0000

    Updating file 4-sum.h to version 1

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,11 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Write a function-like macro SUM(x, y) that computes the sum of the numbers x and y. */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif /* HOLBERTON_H */
