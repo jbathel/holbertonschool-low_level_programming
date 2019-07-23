2b366e2ab5bcc39f5ee67dab352a54460fa947a5
jbathel <811@holbertonschool.com>
2019-07-23 01:48:32 +0000

    Updating file 1-init_dog.c to version 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,11 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "dog.h"
 
-/**
- * print_dog -
- *
- * Return: 
- */
-void print_dog(struct dog *d)
-{
 
+/**
+ * init_dog - calls function
+ * @d: struct name
+ * @name: dog name
+ * @age: age of dog
+ * @owner: owner name
+ * Description: Function that initialize a variable of type struct dog
+ * Return: 0
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
