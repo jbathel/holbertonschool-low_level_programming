b714179e8bbd0b305d4ce70c486ab3eda0182f46
jbathel <811@holbertonschool.com>
2019-07-23 03:55:58 +0000

    Updating file 2-print_dog.cto version 1

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,11 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "dog.h"
 
-/**
- * new_dog -
- *
- * Return: 
- */
-dog_t *new_dog(char *name, float age, char *owner)
-{
 
+/**
+ * init_dog - calls function
+ * @d: struct name
+ * Description: Function that prints a struct dog
+ * Return: 0
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	printf("Name = %s\nAge = %g\nOwner = %s\n", d->name, d->age, d->owner);
+	return;
 }
