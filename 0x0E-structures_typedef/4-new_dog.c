56c577050f0a567008e3ce1990848b33ceba6f7a
jbathel <811@holbertonschool.com>
2019-07-23 04:22:28 +0000

    Updating 4-new_dog.c to version 1

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * *new_dog - calls function
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner name
+ * Description: Function that creates a new dog
+ * Return: 0
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+	printf("Name = %s\nAge = %g\nOwner = %s\n", d->na
+			me, d->age, d->owner);
+}
