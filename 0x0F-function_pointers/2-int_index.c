da23b3b192cadbc0df8e26769bd162eec7127c07
jbathel <jessica.bathel@gmail.com>
2019-07-24 22:10:25 +0000

    Updating file 2-int_index.c to version 1

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+/**
+  * int_index - calls function
+  * @array: passed from main
+  * @size: size of array
+  * @cmp: pointer to function you need to use
+  * Description: Function that searches for a an integer
+  * each element of an array.
+  * Return: 0
+  */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL)
+		return (0);
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		return (i);
+	}
+	return (0);
+}
