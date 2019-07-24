d62c0550801702d2285edfc09ea00c801a0a5c3a
jbathel <jessica.bathel@gmail.com>
2019-07-24 20:54:17 +0000

    Updating 1-array_iterator.c to version 2, iteratoring through the array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,7 +10,10 @@
   */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	unsigned int i;
+
 	if (array == NULL)
 		return;
-	(*action)(array[size]);
+	for (i = 0; i < size; i++)
+		(*action)(array[i]);
 }
