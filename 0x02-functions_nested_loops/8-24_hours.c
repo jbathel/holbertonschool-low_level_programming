bd6137f2574b7cccdbeff6d583df4d4603b12241
jbathel <jessica.bathel@gmail.com>
2019-07-25 19:35:26 +0000

    Updating file 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,20 +8,19 @@
  */
 void jack_bauer(void)
 {
-int h, m;
+	int h, m;
 
-	for (h = 0; h <= 23; h++)
+	for (h = 0; h < 24; h++)
 	{
-	_putchar (h / 10);
-	_putchar (h % 10);
-	_putchar (':');
+		for (m = 0; m < 60; m++)
+		{
+			_putchar ('0' + h / 10);
+			_putchar ('0' + h % 10);
+			_putchar (':');
+			_putchar ('0' + m / 10);
+			_putchar ('0' + m % 10);
+			_putchar('\n');
+		}
 	}
-	for (m = 0; m <= 59; m++)
-	{
-	_putchar (m / 10);
-	_putchar (m % 10);
-	putchar('\n');
-	}
-return (0);
 }
 
