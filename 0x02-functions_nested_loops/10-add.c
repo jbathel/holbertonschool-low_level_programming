be603a10fe1189a433797a551b31ec06c43e0024
jbathel <jessica.bathel@gmail.com>
2019-07-26 05:03:26 +0000

    Uploading file 10-add.c version 1

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,13 @@
 #include "holberton.h"
 
 /**
-  *
+  * add - calls function
+  * @int: integers to be added
+  * Description: Adds two integers
+  * Return: int
+  */
 
-int op_add(int arg1, int arg2)
+int add(int a, int b)
 {
-		return (arg1 + arg2);
+	return (a + b);
 }
