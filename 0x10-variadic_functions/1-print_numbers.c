d08efc1c56ef8463914f887414376b115aa962ef
jbathel <jessica.bathel@gmail.com>
2019-07-27 23:38:35 +0000

    Upating 1-print_numbers.c to version 2 - fixing separator error

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,9 +19,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		args = va_arg(print, int);
+		{
 		if (i != 0)
 		printf("%s", separator);
 		printf("%d", args);
+		}
 	}
 	printf("\n");
 	va_end(print);
