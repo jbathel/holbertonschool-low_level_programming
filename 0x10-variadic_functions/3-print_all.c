d065d57957111a9c9c065aea662a1eba33336b76
jbathel <jessica.bathel@gmail.com>
2019-07-27 23:12:57 +0000

    Updating file 3-print_all.c to version 2 - adding separator and betty formating

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,43 +2,49 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
+
 /**
- * print_all - calls function
- * @format: list of types of arguments passed to the function
- * Description: Function that prints anything.
+ * op_char - calls function
+ * @printAll: parser
  * Return: 0
- */
-/**
- *
  */
 void op_char(va_list printAll)
 {
 	printf("%c", va_arg(printAll, int));
 }
-	/**
-	 *
-	 */
+/**
+ * op_int - calls function
+ * @printAll: parser
+ * Return: 0
+ */
 void op_int(va_list printAll)
 {
 	printf("%d", va_arg(printAll, int));
 }
-	/**
-	 *
-	 */
+/**
+ * op_float - calls function
+ * @printAll: parser
+ * Return: 0
+ */
 void op_float(va_list printAll)
 {
 	printf("%f", va_arg(printAll, double));
 }
-	/**
-	 *
-	 */
+/**
+ * op_char_ptr - calls function
+ * @printAll: parser
+ * Return: 0
+ */
 void op_char_ptr(va_list printAll)
 {
 	printf("%s", va_arg(printAll, char *));
 }
-	/**
-	 *
-	 */
+/**
+ * print_all - calls function
+ * @format: list of types of arguments passed to the function
+ * Description: Function that prints anything
+ * Return: 0
+ */
 void print_all(const char * const format, ...)
 {
 	int i, j;
@@ -62,10 +68,12 @@ void print_all(const char * const format, ...)
 		{
 			if (format[i] == *f_ops[j].ic)
 			{
+				if (j != 0)
+				printf(", ");
 				f_ops[j].ch(printAll);
 
 			}
 		}
 	}
-
+	printf("\n");
 }
