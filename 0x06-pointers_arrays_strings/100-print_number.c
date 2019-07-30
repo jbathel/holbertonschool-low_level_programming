1968b5d3cb84b52178901e0af30c75a1e7c75ef1
jbathel <jessica.bathel@gmail.com>
2019-07-30 17:04:33 +0000

    Uploading file to repo - version 1

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * print_number - calls function
+ * @n: integer passed
+ * @m: integer to account for -
+ * Description: Function that prints an integer
+ * Return: 0
+ */
+void print_number(int n)
+{
+	unsigned int m;
+
+	if (n >= 0)
+	{
+		if (n >= 10)
+			print_number(n / 10);
+		_putchar(n % 10 + '0');
+	}
+	else
+	{
+		_putchar('-');
+		m = -n;
+		if (m >= 10)
+			print_number( m / 10);
+		_putchar(m % 10 + '0');
+	}
+}
