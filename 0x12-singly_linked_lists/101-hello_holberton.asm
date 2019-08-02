3c3821f972881f262cc2b86d9530dde829bf847d
jbathel <jessica.bathel@gmail.com>
2019-08-02 17:56:43 +0000

    Updating .asm file for proper output

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -5,7 +5,7 @@ section .text
 _start:
 mov rax, 1        ; write(
 		mov rdi, 1        ;   STDOUT_FILENO,
-		mov rsi, msg      ;   "Hello, Holberton!\n",
+		mov rsi, msg      ;   "Hello, Holberton\n",
 		mov rdx, msglen   ;   sizeof("Hello, world!\n")
 		syscall           ; );
 
