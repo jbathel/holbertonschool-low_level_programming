def9bc9fe984a23c7aa3507ae0f8d2778b3a3894
jbathel <jessica.bathel@gmail.com>
2019-08-02 05:58:08 +0000

    Updating file to version 1

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - Function that frees a list
+ * @head: pointer to head
+ * Return: 0
+ */
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	while (current != NULL)
+	{
+		current = head->next;
+		free(head->str);
+		free(head);
+		head = current;
+	}
+}
