521c02e730d3d41d2abd5b8178682b63c6ea62fa
jbathel <jessica.bathel@gmail.com>
2019-08-02 06:34:31 +0000

    Updating to version 2

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,19 @@
-global _start
+iglobal _start
 
 section .text
 
 _start:
 mov rax, 1        ; write(
 		mov rdi, 1        ;   STDOUT_FILENO,
-		mov rsi, msg      ;   "Hello, Holberton\n",
-		mov rdx, msglen   ;   sizeof("Hello, Holberton\n")
+		mov rsi, msg      ;   "Hello, Holberton!\n",
+		mov rdx, msglen   ;   sizeof("Hello, world!\n")
 		syscall           ; );
 
 mov rax, 60       ; exit(
 		mov rdi, 0        ;   EXIT_SUCCESS
 		syscall           ; );
 
-section .rodata
-msg: db "Hello, world!", 10
+section .data
+msg: db "Hello, Holberton\n", 10
 msglen: equ $ - msg
+
