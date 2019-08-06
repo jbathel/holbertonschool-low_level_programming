4c5f8c36e47d269e55f64c6e9d4b26389f5aae14
jbathel <jessica.bathel@gmail.com>
2019-08-06 19:50:17 +0000

    Updating file to version 2 - setting counter to 1

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,7 +12,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (head == NULL)
 		return (NULL);
 
-	for (counter = 0; counter <= index; counter++)
+	for (counter = 1; counter <= index; counter++)
 	{
 		if (head->next)
 			head = head->next;
