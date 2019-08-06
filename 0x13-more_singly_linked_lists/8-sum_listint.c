9b7161f2ff0417c7a396a0b93deea0095c77effc
jbathel <jessica.bathel@gmail.com>
2019-08-06 20:16:03 +0000

    Updating to version 1 - recursion

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,9 +1,8 @@
 #include "lists.h"
 /**
- * int sum_listint - Function that returns the sum of all
+ * sum_listint - Function that returns the sum of all
  * the data(n) of a linked list
  * @head: pointer
- * @index: the index of the node starting at 0
  * Return: Sum
  */
 int sum_listint(listint_t *head)
@@ -11,5 +10,5 @@ int sum_listint(listint_t *head)
 	if (head == NULL)
 		return (0);
 
-	return(head->n + sum_listint(head->next));
+	return (head->n + sum_listint(head->next));
 }
