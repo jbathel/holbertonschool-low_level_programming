db8efcb799a140a69dc76cd5ed9182e37850c32e
jbathel <jessica.bathel@gmail.com>
2019-08-06 17:08:38 +0000

    Adding spaces

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,14 +1,16 @@
 #include "lists.h"
+
 /**
  * print_listint - Function that prints all the elements of a list.
  * @h: pointer
+ *
  * Return: the number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
