600d988a8a118089a43e27bbac7c5f9374dfa253
jbathel <jessica.bathel@gmail.com>
2019-08-07 00:41:35 +0000

    Update to version 1

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+/**
+ * *insert_nodeint_at_index - Function that returns the nth node of a list
+ * @head: pointer to a pointer to head
+ * @idx: the index of the node starting at 0
+ * @n: data
+ * Return: 0
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i;
+	listint_t *node;
+	listint_t *temp = *head;
+
+	if (!*head || !head)
+		return (NULL);
+
+	node = malloc(sizeof(listint_t));
+
+	if (node == NULL)
+		return (NULL);
+
+	if (idx == 0)
+	{
+		node->next = (*head);
+		(*head) = node;
+		(*head)->n = n;
+		return (*head);
+	}
+
+	for (i = 0; i < idx - 1 && temp != NULL; i++)
+		temp = temp->next;
+	if (!temp)
+	{
+		free(node);
+		return (NULL);
+	}
+	node->next = temp->next;
+	temp->next = node;
+	node->n = n;
+
+	return (node);
+}
