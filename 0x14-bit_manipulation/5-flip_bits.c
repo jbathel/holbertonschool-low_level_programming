3daa6a18e0e46cec4c40471ddab0a4d823e9d32f
jbathel <jessica.bathel@gmail.com>
2019-08-09 00:02:12 +0000

    Updating to version 1

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+  * flip_bits - Function that returns the number of bits you would need to
+  * flip to get from one number to another
+  * @n: unsigned long int
+  * @m: unsigned long int
+  * Return: 0
+  */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int count = 0;
+	unsigned int zor;
+
+	zor = n ^ m;
+
+	while (zor)
+	{
+		if (zor & 1)
+			count++;
+		zor = zor >> 1;
+	}
+	return (count);
+}
