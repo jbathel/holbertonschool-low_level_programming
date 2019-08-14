f9cef74a2e1797d351b1a02f1c76c76eb864ed5d
jbathel <jessica.bathel@gmail.com>
2019-08-14 00:39:21 +0000

    Fixing typo

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -4,8 +4,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 int _putchar(char c);
-size_t read_textfile(const char *filename, size_t letters);
+ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
