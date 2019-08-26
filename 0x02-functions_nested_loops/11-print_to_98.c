bff7e3efad071970eaf7be825d95cb2f642a532b
jbathel <jessica.bathel@gmail.com>
2019-08-26 02:46:33 +0000

    Version 2 - fixing betty and output errors

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,8 +2,7 @@
 #include <stdio.h>
 /**
  * print_to_98 - calls function
- *
- *
+ * @n: integer passed from main
  * Description: Function that prints all natural numbers from
  * n to 98, follwed by a newline.
  * Return: 0
@@ -12,22 +11,24 @@ void print_to_98(int n)
 {
 	int i = n;
 
-	if (n < 98)
+	if (n <= 98)
 	{
-		for (; i <= 98; i++)
+		for (; i <= 98; ++i)
 		{
-			if (i < 98)
+			if (i != 98)
 				printf("%d, ", i);
-			else if (i == 98)
+			if (i == 98)
 				printf("%d\n", i);
 		}
 	}
-	else if (n > 98)
+	if (n > 98)
 	{
-		for (; i >= 98; i--)
-			printf("%d, ", i);
+		for (; i >= 98; --i)
+		{
+			if (i == 98)
+				printf("%d\n", i);
+			if (i != 98)
+				printf("%d, ", i);
+		}
 	}
-	else
-		printf("98\n");
-	return;
 }
