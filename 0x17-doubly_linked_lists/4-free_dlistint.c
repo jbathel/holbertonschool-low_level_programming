f911646c1d420c805d7bd92ce07b99e05579d35f
jbathel <jessica.bathel@gmail.com>
2019-09-15 23:52:06 +0000

    Version 1

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,10 +1,18 @@
 #include "lists.h"
 /**
-  * free_dlistint - Calls Function
-  * @head: Pointer to head
-  * Description: Function that frees a list
-  * Return: 0
-  */
+ * free_dlistint - Calls Function
+ * @head: Pointer to head
+ * Description: Function that frees a list
+ * Return: 0
+ */
 void free_dlistint(dlistint_t *head)
 {
+	dlistint_t *current;
+
+	while (head != NULL)
+	{
+		current = head->next;
+		free(head);
+		head = current;
+	}
 }
