5436f1b4f07728de0fdba3070157aa38b4c11a5e
jbathel <jessica.bathel@gmail.com>
2019-09-16 04:01:15 +0000

    Version 1

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,10 +1,14 @@
 #include "lists.h"
 /**
-  * sum_dlistint - Calls Function
-  * @head: Pointer to head
-  * Description: Function that returns the sum of all the data (n) of a list
-  * Return: Sum, else return 0 if list is empty
-  */
+ * sum_dlistint - Calls Function
+ * @head: Pointer to head
+ * Description: Function that returns the sum of all the data (n) of a list
+ * Return: Sum, else return 0 if list is empty
+ */
 int sum_dlistint(dlistint_t *head)
 {
+	if (head == NULL)
+		return (0);
+
+	return (head->n + sum_dlistint(head->next));
 }
