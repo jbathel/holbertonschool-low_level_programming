b3de42d7fe75e844e5d551811ef67998f68fc96f
jbathel <jessica.bathel@gmail.com>
2019-09-16 04:05:55 +0000

    Version 1

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,12 +1,44 @@
 #include "lists.h"
 /**
-  * *insert_dnodeint_at_index - Calls Function
-  * @h: Pointer to pointer h
-  * @idx: integer index
-  * @n: integer
-  * Description: Function that inserts a new node at a given position
-  * Return: the address of the new node, or NULL if it failed
-  */
+ * *insert_dnodeint_at_index - Calls Function
+ * @h: Pointer to pointer h
+ * @idx: integer index
+ * @n: integer
+ * Description: Function that inserts a new node at a given position
+ * Return: the address of the new node, or NULL if it failed
+ */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+	unsigned int i;
+	dlistint_t *node;
+	dlistint_t *temp = *h;
+
+	if (idx != 0 && (h == NULL || (*h) == NULL))
+		return (NULL);
+
+	node = malloc(sizeof(dlistint_t));
+
+	if (node == NULL)
+		return (NULL);
+
+	if (idx == 0 && (h != NULL || (*h) != NULL))
+	{
+		node->next = (*h);
+		(*h) = node;
+		(*h)->n = n;
+		return (*h);
+	}
+
+	for (i = 0; i < idx - 1 && temp != NULL; i++)
+		temp = temp->next;
+	if (!temp)
+	{
+		free(node);
+		return (NULL);
+	}
+	node->next = temp->next;
+	temp->next = node;
+	node->n = n;
+
+	return (node);
 }
