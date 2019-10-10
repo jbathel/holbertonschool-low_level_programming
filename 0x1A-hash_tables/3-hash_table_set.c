3e564c07e330903b5874f72d4d6cdde56815026d
Jessica Bathel <811@holbertonschool.com>
2019-10-10 21:19:23 +0000

    Version 1

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,11 +1,26 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set -
- *
- * Return: 
+ * hash_table_set - Function that adds an element to the table
+ * @key: key
+ * @value: value
+ * Return: 1 on Success 0 if Fails
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	unsigned long int index, size;
+	hash_node_t *node;
 
+	node = malloc(sizeof(hash_node_t));
+
+	if (node == NULL)
+		return (0);
+
+	size = ht->size;
+	index = key_index((unsigned char *)key, size);
+	ht->array[index] = node;
+	node->key = (char *)key;
+	node->value = (char *)value;
+
+	return (1);
 }
