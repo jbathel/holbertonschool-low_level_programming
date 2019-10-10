5627f3edebd7096e92f65772ea053db468aa1a99
Jessica Bathel <811@holbertonschool.com>
2019-10-10 21:34:48 +0000

    Version 2 - Adding checks for empty nodes

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,6 +11,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index, size;
 	hash_node_t *node;
 
+	if (strcmp(key, "") == 0 || !key)
+		return (0);
+
 	node = malloc(sizeof(hash_node_t));
 
 	if (node == NULL)
@@ -18,9 +21,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	size = ht->size;
 	index = key_index((unsigned char *)key, size);
-	ht->array[index] = node;
 	node->key = (char *)key;
 	node->value = (char *)value;
-
+	if (ht->array[index] != NULL)
+	{
+		node->next = ht->array[index];
+		ht->array[index] = node;
+		return (1);
+	}
+	ht->array[index] = node;
 	return (1);
 }
