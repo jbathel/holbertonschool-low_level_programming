cbdc0319f569004edecfda5907e3f04da83f6452
Jessica Bathel <811@holbertonschool.com>
2019-10-11 22:50:39 -0700

    Version 2 - Fixing betty error

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -10,22 +10,22 @@ void hash_table_delete(hash_table_t *ht)
 	unsigned long int index;
 	hash_node_t *temp;
 
-    if (ht == NULL)
-        return;
+	if (ht == NULL)
+		return;
 
 	for (index = 0; index < ht->size; index++)
-        if (ht->array[index] != NULL)
-        {
-            while (ht->array[index] != NULL)
-            {
-                temp = ht->array[index]->next;
-                free(ht->array[index]->key);
-                free(ht->array[index]->value);
-                free(ht->array[index]);
-                ht->array[index] = temp;
-            }
-            free(ht->array[index]);
-        }
-    free(ht->array);
-    free(ht);
+		if (ht->array[index] != NULL)
+		{
+			while (ht->array[index] != NULL)
+			{
+				temp = ht->array[index]->next;
+				free(ht->array[index]->key);
+				free(ht->array[index]->value);
+				free(ht->array[index]);
+				ht->array[index] = temp;
+			}
+			free(ht->array[index]);
+		}
+	free(ht->array);
+	free(ht);
 }
