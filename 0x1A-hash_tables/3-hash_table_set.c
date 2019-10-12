4e4288039eee6221ee67fd3f0757b02c4ae6815c
Jessica Bathel <811@holbertonschool.com>
2019-10-11 17:55:46 -0700

    Adding free statements

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,36 +1,47 @@
 #include "hash_tables.h"
-
 /**
  * hash_table_set - Function that adds an element to the table
+ * @ht: hash table array
  * @key: key
  * @value: value
  * Return: 1 on Success 0 if Fails
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index, size;
+	unsigned long int index;
 	hash_node_t *node;
+	hash_node_t *new_node;
 
-	if (strcmp(key, "") == 0 || !key)
+	if (!ht || strcmp(key, "") == 0 || !key || !value)
 		return (0);
 
-	node = malloc(sizeof(hash_node_t));
-
-	if (node == NULL)
+	index = key_index((unsigned char *)key, ht->size);
+	node = ht->array[index];
+	/* Creating new node */
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
 		return (0);
-
-	size = ht->size;
-	index = key_index((unsigned char *)key, size);
-	ht->array[index] = node;
-	node->key = strdup(key);
-	node->value = strdup(value);
-	while (ht->array[index] != NULL)
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	if (!new_node->key || !new_node->value)
 	{
-		node->next = ht->array[index];
-		ht->array[index] = node;
-		node->key = strdup(key);
-		node->value = strdup(value);
-		return (1);
+		free(new_node);
+		return (0);
 	}
+	if (node != NULL)
+	{
+		while (node)
+		{
+			if (strcmp(key, node->key) == 0)
+			{
+				free(node->value);
+				node->value =  strdup((char *)value);
+				return (1);
+			}
+			node = node->next;
+		}
+	}
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
 	return (1);
 }
