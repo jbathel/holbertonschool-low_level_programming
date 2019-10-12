c0a87f7536e3a37173b79f5c2baf55ef62443cf3
Jessica Bathel <811@holbertonschool.com>
2019-10-11 20:49:07 -0700

    Version 1

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,11 +1,18 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_print -
- *
- * Return: 
+ * hash_table_print - Function that prints a hash table
+ * @ht: hash table to print
+ * Return: 0
  */
 void hash_table_print(const hash_table_t *ht)
 {
+	unsigned long int index;
 
+	if (ht == NULL)
+		printf("{}");
+
+	for (index = 0; index < ht->size; index++)
+		if (ht->array[index] != NULL)
+			printf("{%s : %s}\n", ht->array[index]->key, ht->array[index]->value);
 }
