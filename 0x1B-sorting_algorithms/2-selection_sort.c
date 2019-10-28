99988b62c5933f2237709619f4344dfe6f674f69
Jessica Bathel <811@holbertonschool.com>
2019-10-28 19:58:32 +0000

    Fixing betty errors and adding swap to file

--- 0x1B-sorting_algorithms/2-selection_sort.c
+++ 0x1B-sorting_algorithms/2-selection_sort.c
@@ -1,22 +1,41 @@
 #include "sort.h"
 
 /**
- * selection_sort -
- *
- * Return:
+ * selection_sort - Calls function
+ * @array: Array to be sorted
+ * @size: Size of array given
+ * Descrtiption: Function that sorts an array using the Selection
+ * sort algotrithm
+ * Return: 0
  */
 void selection_sort(int *array, size_t size)
 {
-    unsigned int i, j, min_idx;
+	unsigned int i, j, min_idx;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        min_idx = i;
-        for (j = i + 1; j < size; j++)
-          if (array[j] < array[min_idx])
-            min_idx = j;
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
+			if (array[j] < array[min_idx])
+				min_idx = j;
 
-        swap(&array[min_idx], &array[i]);
-	print_array(array, size);
-    }
+		swap(&array[min_idx], &array[i]);
+		print_array(array, size);
+	}
+}
+
+/**
+ * swap - Function that swaps two values
+ *
+ * @a: Fisrt value
+ * @b: Second value
+ * Return: 0
+ */
+void swap(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *b;
+	*b = *a;
+	*a = tmp;
 }
