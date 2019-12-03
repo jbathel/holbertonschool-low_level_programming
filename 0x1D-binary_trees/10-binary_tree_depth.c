3830c2f4ad57b0d453cad15dcaa8c92ea45c7561
Jessica Bathel <811@holbertonschool.com>
2019-12-03 23:15:32 +0000

    Version 1 - NOT betty compliant

--- 0x1D-binary_trees/10-binary_tree_depth.c
+++ 0x1D-binary_trees/10-binary_tree_depth.c
@@ -1,11 +1,23 @@
 #include "binary_trees.h"
 
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
 /**
- * binary_tree_depth -
- *
- * Return: 
+ * _binary_tree_depth - Function that measures the depth of a node in a binary tree
+ * @tree: Pointer to root node
+ * Return: Depth of the node, if tree is NULL return 0
+ */
+size_t _binary_tree_depth(const binary_tree_t *tree)
+{
+    return (tree ? _binary_tree_depth(tree->parent) + 1 : 0);
+}
+
+/**
+ * binary_tree_depth - Function that measures the depth of a node in a binary tree
+ * @tree: Pointer to root node
+ * Return: Depth of the node, if tree is NULL return 0
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-
+    return (tree ? _binary_tree_depth(tree->parent) : 0);
 }
--- 0x1D-binary_trees/11-binary_tree_size.c
+++ 0x1D-binary_trees/11-binary_tree_size.c
@@ -1,11 +1,15 @@
 #include "binary_trees.h"
 
+#define SUM(a, b) ((a) + (b))
 /**
- * binary_tree_size -
- *
- * Return: 
+ * binary_tree_size - Function that measures the size of a binary tree
+ * @tree: Pointer to the root node of the tree
+ * Return: Size of tree, if tree is NULL return 0
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-
+    return (tree ? SUM(
+                binary_tree_size(tree->left),
+                binary_tree_size(tree->right)
+                ) + 1 : 0);
 }
