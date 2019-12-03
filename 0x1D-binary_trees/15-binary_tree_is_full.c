d4a08b7d87b3beff83ad66cae690d22906a3da84
Jessica Bathel <811@holbertonschool.com>
2019-12-03 23:47:11 +0000

    Version 1 - NOT betty compliant

--- 0x1D-binary_trees/15-binary_tree_is_full.c
+++ 0x1D-binary_trees/15-binary_tree_is_full.c
@@ -1,11 +1,21 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_full -
- *
- * Return: 
+ * binary_tree_is_full - Function that checks if a binary tree is full
+ * @tree: Pointer to the root node of the tree
+ * Return: 1, if tree is NULL return 0
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
+    if (!tree)
+        return (0);
 
+    if (!tree->left)
+        return (!tree->right);
+
+    if (!tree->right)
+        return (!tree->left);
+
+    return (binary_tree_is_full(tree->left) &&
+            binary_tree_is_full(tree->right));
 }
