7a92e6f629115bf8a421e250e549d641a93d0f45
Jessica Bathel <811@holbertonschool.com>
2019-12-03 23:02:33 +0000

    Version 1 - NOT betty compliant

--- 0x1D-binary_trees/9-binary_tree_height.c
+++ 0x1D-binary_trees/9-binary_tree_height.c
@@ -1,11 +1,29 @@
 #include "binary_trees.h"
 
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
 /**
- * binary_tree_height -
- *
- * Return: 
+ * _binary_tree_height - Function that measures the height of binary tree
+ * @tree: Pointer to the root of the tree
+ * Return: Height of tree, if tree is NULL return 0
+ */
+size_t _binary_tree_height(const binary_tree_t *tree)
+{
+    return (tree ? MAX(
+                _binary_tree_height(tree->left),
+                _binary_tree_height(tree->right)
+                ) + 1 : 0);
+}
+
+/**
+ * binary_tree_height - Function that measures the height of binary tree
+ * @tree: Pointer to the root of the tree
+ * Return: Height of tree, if tree is NULL return 0
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-
+    return (tree ? MAX(
+                _binary_tree_height(tree->left),
+                _binary_tree_height(tree->right)
+                ) : 0);
 }
