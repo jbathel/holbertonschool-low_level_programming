3debbac69afb0644679f2455374618aa172f0286
Jessica Bathel <811@holbertonschool.com>
2019-12-03 22:11:06 +0000

    Version 1 - NOT betty compliant

--- 0x1D-binary_trees/6-binary_tree_preorder.c
+++ 0x1D-binary_trees/6-binary_tree_preorder.c
@@ -1,11 +1,29 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder -
- *
- * Return: 
+ * binary_tree_preorder - Function that uses pre-order traversal
+ * @tree: Pointer to the root node of the tree
+ * @func: Pointer tot he function to call
+ * Return: 0
  */
+void _binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+    if (tree)
+    {
+        func(tree->n);
+        _binary_tree_preorder(tree->left, func);
+        _binary_tree_preorder(tree->right, func);
+    }
+}
+
+/**
+ * binary_tree_preorder - Function that uses pre-order traversal
+ * @tree: Pointer to the root node of the tree
+ * @func: Pointer tot he function to call
+ * Return: 0
+*/
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-
+    if (func)
+        _binary_tree_preorder(tree, func);
 }
--- 0x1D-binary_trees/7-binary_tree_inorder.c
+++ 0x1D-binary_trees/7-binary_tree_inorder.c
@@ -1,11 +1,28 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_inorder -
- *
- * Return: 
+ * binary_tree_inorder - Function that uses in-order traversal
+ * @tree: Pointer to the root node of the tree
+ * @func: Pointer to the function to call
+ * Return: 0
+ */
+void _binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+    if (tree)
+    {
+        _binary_tree_inorder(tree->left, func);
+        func(tree->n);
+        _binary_tree_inorder(tree->right, func);
+    }
+}
+/**
+ * binary_tree_inorder - Function that uses in-order traversal
+ * @tree: Pointer to the root node of the tree
+ * @func: Pointer to the function to call
+ * Return: 0
  */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-
+    if (func)
+        _binary_tree_inorder(tree, func);
 }
