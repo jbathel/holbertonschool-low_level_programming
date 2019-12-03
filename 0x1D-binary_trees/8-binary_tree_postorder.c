4a5b73f95309ea6236e881a68754a4f76b230ef7
Jessica Bathel <811@holbertonschool.com>
2019-12-03 22:48:41 +0000

    Version 1 - NOT betty compliant

--- 0x1D-binary_trees/8-binary_tree_postorder.c
+++ 0x1D-binary_trees/8-binary_tree_postorder.c
@@ -1,11 +1,29 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder -
- *
- * Return: 
+ * binary_tree_postorder - Function that uses post-order traversal
+ * @tree: Pointer to root of the tree
+ * @func: Pointer to the function to call
+ * Return: 0
+ */
+void _binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+    if (tree)
+    {
+        _binary_tree_postorder(tree->left, func);
+        _binary_tree_postorder(tree->right, func);
+        func(tree->n);
+    }
+}
+
+/**
+ * binary_tree_postorder - Function that uses post-order traversal
+ * @tree: Pointer to root of the tree
+ * @func: Pointer to the function to call
+ * Return: 0
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-
+    if (func)
+        _binary_tree_postorder(tree, func);
 }
