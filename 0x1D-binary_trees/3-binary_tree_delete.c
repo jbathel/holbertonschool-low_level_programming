00be7009d2233ca67e4a266d5fd9a771fc8b0e44
Jessica Bathel <811@holbertonschool.com>
2019-12-03 21:13:01 +0000

    Version 1 - NOT betty compliant

--- 0x1D-binary_trees/3-binary_tree_delete.c
+++ 0x1D-binary_trees/3-binary_tree_delete.c
@@ -1,11 +1,16 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_delete -
- *
- * Return: 
+ * binary_tree_delete - Recursive function that deletes an entire binary tree
+ * @tree: Pointer to the root node of the tree
+ * Return: 0
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-
+    if (tree)
+    {
+        binary_tree_delete(tree->left);
+        binary_tree_delete(tree->right);
+        free(tree);
+    }
 }
