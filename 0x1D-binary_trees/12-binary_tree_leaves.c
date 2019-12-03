b2d5078b63ad2aced291bf9ed3776bb34d27243f
Jessica Bathel <811@holbertonschool.com>
2019-12-03 23:30:41 +0000

    Version 1 - NOT betty compliant

--- 0x1D-binary_trees/12-binary_tree_leaves.c
+++ 0x1D-binary_trees/12-binary_tree_leaves.c
@@ -1,11 +1,22 @@
 #include "binary_trees.h"
 
+#define SUM(a, b) ((a) + (b))
+
 /**
- * binary_tree_leaves -
- *
- * Return: 
+ * binary_tree_leaves - Function that counts the leaves in a binary tree
+ * @tree: Pointer to the root node of the tree
+ * Return: Number of leaves, if tree is NULL return 0
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+    if (!tree)
+        return (0);
 
+    if (!(tree->left || tree->right))
+        return (1);
+
+    return (SUM(
+                binary_tree_leaves(tree->left),
+                binary_tree_leaves(tree->right)
+               ));
 }
