71929b2d326e6d85de19b178f7847cf67b4fe467
Jessica Bathel <811@holbertonschool.com>
2019-12-03 23:41:55 +0000

    Version 1 - NOT betty compliant

--- 0x1D-binary_trees/14-binary_tree_balance.c
+++ 0x1D-binary_trees/14-binary_tree_balance.c
@@ -1,11 +1,30 @@
 #include "binary_trees.h"
 
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define DIF(a, b) ((a) - (b))
+
 /**
- * binary_tree_balance -
- *
- * Return: 
+ * _binary_tree_balance - Function that measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree
+ * Return: Height of the tree, if tree is NULL return 0
+ */
+int _binary_tree_height(const binary_tree_t *tree)
+{
+    return (tree ? MAX(
+                _binary_tree_height(tree->left),
+                _binary_tree_height(tree->right)
+                ) + 1 : 0);
+}
+
+/**
+ * binary_tree_balance - Function that measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree
+ * Return: Height of the tree, if tree is NULL return 0
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-
+    return (tree ? DIF(
+                _binary_tree_height(tree->left),
+                _binary_tree_height(tree->right)
+                ) : 0);
 }
