176e7a24796d6500248b13fd756d3d29f6260593
Jessica Bathel <811@holbertonschool.com>
2019-12-03 21:21:29 +0000

    Version 1 - NOT betty compliant

--- 0x1D-binary_trees/4-binary_tree_is_leaf.c
+++ 0x1D-binary_trees/4-binary_tree_is_leaf.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf -
- *
- * Return: 
+ * binary_tree_is_leaf - Function that checks if a given node is a leaf
+ * @node: Pointer to the node to check
+ * Return: 1 if node is a leaf, otherwise 0
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-
+    return (node && !node->left && !node->right);
 }
