2fcd882968c6a98b042c3bc80060b78d8fb02754
Jessica Bathel <811@holbertonschool.com>
2019-12-04 02:35:23 +0000

    Version 1 - NOT betty compliant

--- 0x1D-binary_trees/17-binary_tree_sibling.c
+++ 0x1D-binary_trees/17-binary_tree_sibling.c
@@ -1,11 +1,19 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_sibling -
- *
- * Return: 
+ * binary_tree_sibling - Function that finds the sibling of a node
+ * @node: Pointer to node
+ * Return: Pointer to the sibling, if node is NULL or does not have a sibling return NULL
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
+    if (node && node->parent)
+    {
+        if (node == node->parent->left)
+            return (node->parent->right);
 
+        if (node == node->parent->right)
+            return (node->parent->left);
+    }
+    return (NULL);
 }
