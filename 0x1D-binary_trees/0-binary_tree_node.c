c32485ecbbfa7f1ff9c9a6cf044fb98c61f6935c
Jessica Bathel <811@holbertonschool.com>
2019-12-04 23:24:34 +0000

    Betty Fix

--- 0x1D-binary_trees/0-binary_tree_node.c
+++ 0x1D-binary_trees/0-binary_tree_node.c
@@ -9,14 +9,14 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node = malloc(sizeof(*new_node));
+	binary_tree_t *new_node = malloc(sizeof(*new_node));
 
-    if (new_node)
-    {
-        new_node->n = value;
-        new_node->parent = parent;
-        new_node->left = NULL;
-        new_node->right = NULL;
-    }
-    return (new_node);
+	if (new_node)
+	{
+		new_node->n = value;
+		new_node->parent = parent;
+		new_node->left = NULL;
+		new_node->right = NULL;
+	}
+	return (new_node);
 }
