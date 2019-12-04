8b1e19b4db63319b0530f574a4d3e78bd8c119f6
Jessica Bathel <811@holbertonschool.com>
2019-12-04 23:32:29 +0000

    Betty fix

--- 0x1D-binary_trees/1-binary_tree_insert_left.c
+++ 0x1D-binary_trees/1-binary_tree_insert_left.c
@@ -1,27 +1,28 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - Function that inserts a new node as the left child of a node
+ * binary_tree_insert_left - Function that inserts a
+ * new node as the left child of a node
  * @parent: Pointer to the parent of the node to be created
  * @value: Value inserted into the new node
  * Return: A pointer to the new node, unless fails then return NULL
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new = NULL;
+	binary_tree_t *new = NULL;
 
-    if (parent)
-    {
-        new = binary_tree_node(parent, value);
-        if (new)
-        {
-            if (parent->left)
-            {
-                new->left = parent->left;
-                new->left->parent = new;
-            }
-            parent->left = new;
-        }
-    }
-    return (new);
+	if (parent)
+	{
+		new = binary_tree_node(parent, value);
+		if (new)
+		{
+			if (parent->left)
+			{
+				new->left = parent->left;
+				new->left->parent = new;
+			}
+			parent->left = new;
+		}
+	}
+	return (new);
 }
