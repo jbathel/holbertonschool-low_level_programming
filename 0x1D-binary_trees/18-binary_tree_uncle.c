6a650c193c7cc49b9f2f30f8baefadef0e4681be
Jessica Bathel <811@holbertonschool.com>
2019-12-05 00:28:54 +0000

    Betty fix

--- 0x1D-binary_trees/18-binary_tree_uncle.c
+++ 0x1D-binary_trees/18-binary_tree_uncle.c
@@ -3,19 +3,20 @@
 /**
  * binary_tree_sibling - Function that fins the sibling of a given node
  * @node: Pointer to the node
- * Return: Pointer to the sibling, if node is NULL or does not have a sibling return NULL
+ * Return: Pointer to the sibling, if node is NULL or
+ * does not have a sibling return NULL
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-    if (node && node->parent)
-    {
-        if (node == node->parent->left)
-            return (node->parent->right);
+	if (node && node->parent)
+	{
+		if (node == node->parent->left)
+			return (node->parent->right);
 
-        if (node == node->parent->right)
-            return (node->parent->left);
-    }
-    return (NULL);
+		if (node == node->parent->right)
+			return (node->parent->left);
+	}
+	return (NULL);
 }
 
 /**
@@ -25,5 +26,5 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-    return (node ? binary_tree_sibling(node->parent) : NULL);
+	return (node ? binary_tree_sibling(node->parent) : NULL);
 }
