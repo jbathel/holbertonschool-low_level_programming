fdb95f6bce7e426e8657e8f68d21f5e7eb8e989b
Jessica Bathel <811@holbertonschool.com>
2019-12-05 00:55:06 +0000

    Updated file to have correct prototypes

--- 0x1D-binary_trees/binary_trees.h
+++ 0x1D-binary_trees/binary_trees.h
@@ -1,6 +1,7 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -18,7 +19,6 @@ typedef struct binary_tree_s
     struct binary_tree_s *parent;
     struct binary_tree_s *left;
     struct binary_tree_s *right;
-
 } binary_tree_t;
 
 typedef struct binary_tree_s bst_t;
@@ -27,14 +27,15 @@ typedef struct binary_tree_s heap_t;
 
 /* Print Binary Trees */
 void binary_tree_print(const binary_tree_t *);
+
+/* Prototypes for Mandatory Tasks */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
 int binary_tree_is_root(const binary_tree_t *node);
-void _binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
-void _binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
-void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_height(const binary_tree_t *tree);
@@ -47,11 +48,6 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
-
-/* Prototypes for Mandatory Tasks */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-
 /* Prototypes for Advanced Tasks */
 
 #endif /* BINARY_TREES_H */
