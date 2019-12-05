9604953cf6dcba30fec038a73dfb5e431b06e547
Jessica Bathel <811@holbertonschool.com>
2019-12-05 00:00:07 +0000

    Betty fix

--- 0x1D-binary_trees/6-binary_tree_preorder.c
+++ 0x1D-binary_trees/6-binary_tree_preorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - Function that uses pre-order traversal
+ * _binary_tree_preorder - Function that uses pre-order traversal
  * @tree: Pointer to the root node of the tree
  * @func: Pointer tot he function to call
  * Return: 0
