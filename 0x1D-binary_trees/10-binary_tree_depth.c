3b1644db50df3e9b39ac9e72da7db1679255240c
Jessica Bathel <811@holbertonschool.com>
2019-12-05 00:09:32 +0000

    Betty fix

--- 0x1D-binary_trees/10-binary_tree_depth.c
+++ 0x1D-binary_trees/10-binary_tree_depth.c
@@ -1,23 +1,20 @@
 #include "binary_trees.h"
 
-#define MAX(a, b) ((a) > (b) ? (a) : (b))
-
 /**
- * _binary_tree_depth - Function that measures the depth of a node in a binary tree
- * @tree: Pointer to root node
- * Return: Depth of the node, if tree is NULL return 0
- */
-size_t _binary_tree_depth(const binary_tree_t *tree)
-{
-    return (tree ? _binary_tree_depth(tree->parent) + 1 : 0);
-}
-
-/**
- * binary_tree_depth - Function that measures the depth of a node in a binary tree
- * @tree: Pointer to root node
- * Return: Depth of the node, if tree is NULL return 0
+ * binary_tree_depth - measure the depth of a node in a binary tree
+ * @tree: a pointer to the node
+ *
+ * Return: If tree is NULL, return 0.
+ * Otherwise, return the depth of the node.
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    return (tree ? _binary_tree_depth(tree->parent) : 0);
+	size_t depth = 0;
+
+	if (tree)
+	{
+		while ((tree = tree->parent))
+			++depth;
+	}
+	return (depth);
 }
