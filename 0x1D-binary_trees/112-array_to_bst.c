de776b7bfeb26751e72dbef68cf22d0eedb98c1e
Jessica Bathel <811@holbertonschool.com>
2019-12-06 21:42:33 +0000

    Version 1

--- 0x1D-binary_trees/112-array_to_bst.c
+++ 0x1D-binary_trees/112-array_to_bst.c
@@ -1,11 +1,21 @@
 #include "binary_trees.h"
 
 /**
- * array_to_bst -
+ * array_to_bst - build a BST from an array
+ * @array: the array from which to construct a BST
+ * @size: the size of the array
  *
- * Return: 
+ * Return: If memory allocation fails, return NULL.
+ * Otherwise, return a pointer to the root of the new BST
  */
 bst_t *array_to_bst(int *array, size_t size)
 {
+	bst_t *root = NULL;
 
+	if (array)
+	{
+		while (size-- && bst_insert(&root, *array++))
+			;
+	}
+	return (root);
 }
