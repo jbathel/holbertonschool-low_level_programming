13ceb87653ad7ff68e8f74147ca230ecf0e4e4dc
Jessica Bathel <811@holbertonschool.com>
2019-12-06 00:50:58 +0000

    Updating function

--- 0x1D-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1D-binary_trees/16-binary_tree_is_perfect.c
@@ -1,15 +1,14 @@
-nclude "binary_trees.h"
+#include "binary_trees.h"
 
 /**
  * binary_tree_is_leaf - checks if a given node is a leaf
  * @node: a pointer to the node to check
  *
- * return: 1 if node is a leaf, otherwise 0
+ * Return: 1 if node is a leaf, otherwise 0
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	return (node && !node->left && !node->right);
-
 }
 
 /**
