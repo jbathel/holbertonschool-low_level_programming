a5b52c7a3e819889d95856fc0cf75334455d06ce
Jessica Bathel <811@holbertonschool.com>
2019-12-06 21:24:32 +0000

    Version 1

--- 0x1D-binary_trees/110-binary_tree_is_bst.c
+++ 0x1D-binary_trees/110-binary_tree_is_bst.c
@@ -1,11 +1,37 @@
 #include "binary_trees.h"
+#include <limits.h>
 
 /**
- * binary_tree_is_bst -
+ * _binary_tree_is_bst - determine if a binary tree is a BST
+ * @tree: the tree to examine
+ * @lower: Value lower
+ * @upper: Value upper
+ * Return: If tree is NULL or is not a BST, return 0.
+ * Otherwise, return 1.
+ */
+int _binary_tree_is_bst(const binary_tree_t *tree, int lower, int upper)
+{
+	if (tree)
+	{
+		if (tree->n > lower && tree->n < upper)
+			return (_binary_tree_is_bst(tree->left, lower, tree->n) &&
+					_binary_tree_is_bst(tree->right, tree->n, upper));
+		return (0);
+	}
+	return (1);
+}
+
+/**
+ * binary_tree_is_bst - determine if a binary tree is a BST
+ * @tree: the tree to examine
  *
- * Return: 
+ * Return: If tree is NULL or is not a BST, return 0.
+ * Otherwise, return 1.
  */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-
+	if (tree)
+		return (_binary_tree_is_bst(tree->left, INT_MIN, tree->n) &&
+				_binary_tree_is_bst(tree->right, tree->n, INT_MAX));
+	return (0);
 }
