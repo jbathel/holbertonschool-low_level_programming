c22a475bfe2f15871d8197b1f61a7489a5b119eb
Jessica Bathel <811@holbertonschool.com>
2019-12-06 18:20:30 +0000

    Version 1

--- 0x1D-binary_trees/103-binary_tree_rotate_left.c
+++ 0x1D-binary_trees/103-binary_tree_rotate_left.c
@@ -1,11 +1,27 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_rotate_left -
- *
- * Return: 
+ * binary_tree_rotate_left - Function that performs a left
+ * rotation on a binary tree
+ * @tree: Pointer to the tree to rotate
+ * Return: Pointer to the new tree root
  */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
+	bt_t *root = NULL;
 
+	if (tree)
+	{
+		root = tree->right;
+		if (root)
+		{
+			tree->right = tree->left;
+			if (tree->right)
+				tree->right->parent = tree;
+			root->left = tree;
+			tree->parent = root;
+			root->parent = NULL;
+		}
+	}
+	return (root);
 }
