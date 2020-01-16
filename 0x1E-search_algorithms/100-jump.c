ab013fe0e0aa0d15a756d16125d94047fad4be74
Jessica Bathel <811@holbertonschool.com>
2020-01-16 22:05:17 +0000

    Fixed betty and moved check if array[i] == value

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -18,11 +18,6 @@ int jump_search(int *array, size_t size, int value)
 	if (!array)
 		return (-1);
 
-	if (array[i] == value)
-	{
-		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-		return (i);
-	}
 	while (array[i] < value)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
@@ -38,6 +33,11 @@ int jump_search(int *array, size_t size, int value)
 		i++;
 		if (i == size)
 			return (-1);
+	}
+	if (array[i] == value)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		return (i);
 	}
 	return (-1);
 }
