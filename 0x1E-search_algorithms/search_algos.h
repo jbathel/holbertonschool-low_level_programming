831de3f74c57e164daab400836bca65d52af08b0
Jessica Bathel <811@holbertonschool.com>
2020-01-16 22:13:57 +0000

    Updated for exponetial search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -11,6 +11,7 @@ int binary_search(int *array, size_t size, int value);
 /* Prototypes for Advanced Tasks */
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
 
 /* Helper Functions */
 void print_array(int *a, size_t l, size_t r);
